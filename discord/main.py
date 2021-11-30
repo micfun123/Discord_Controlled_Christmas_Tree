5cbdae229302749e867267d7d9a535c6c4e17d3b
micfun123 <54761507+micfun123@users.noreply.github.com>
2021-11-30 18:58:49 +0400

    Update main.py

--- discord/main.py
+++ discord/main.py
@@ -1,5 +1,4 @@
 
-# Simple test for NeoPixels on Raspberry Pi
 import time
 import board
 import neopixel
@@ -19,6 +18,28 @@ pixels = neopixel.NeoPixel(
 )
 
 
+#wheel for cycles 
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos * 3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos * 3)
+        g = 0
+        b = int(pos * 3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos * 3)
+        b = int(255 - pos * 3)
+    return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
+
 def rainbow_cycle(wait):
     for j in range(255):
         for i in range(num_pixels):
@@ -27,15 +48,52 @@ def rainbow_cycle(wait):
         pixels.show()
         time.sleep(wait)
 
+def red(): 
+     pixels.fill((0, 0, 0))
+     pixels.show()
+     pixels.fill((255, 0, 0))
+     pixels.show()
+
+def blue(): 
+     pixels.fill((0, 0, 0))
+     pixels.show()
+     pixels.fill((0, 0, 255))
+     pixels.show()
+
+def green(): 
+     pixels.fill((0, 0, 0))
+     pixels.show()
+     pixels.fill((0, 255, 0))
+     pixels.show()
+
 from discord.ext import commands
-        
-client = commands.Bot(command_prefix = '.', intents=intents, presences = True, members = True, guilds=True)
+
+client = commands.Bot(command_prefix = "#", presences = True, members = True, guilds=True)
 
 @client.event
 async def on_ready():
-    # Setting `Playing ` status
-    await client.change_presence(activity=discord.Game(name=".help is a thing")) # changed from bot - client
-    print("we have powered on, I an alive.")
+    await client.change_presence(activity=discord.Game(name = "#help"))
+    print("We have power")
+
+
+@client.command()
+async def red(ctx):
+    await ctx.sent("It is red")
+    red()
+
+
+@client.command()
+async def blue(ctx):
+    await ctx.sent("It is blue")
+    blue()
+
+
+@client.command()
+async def green(ctx):
+    await ctx.sent("It is green")
+    green()
 
 
 
+
+client.run('TOKEN')
