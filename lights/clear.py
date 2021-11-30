cd4539e70a14bd624e65de8d1134d223e59f52b8
micfun123 <michaelrbparker@gmail.com>
2021-11-30 17:49:05 +0400

    clear

---
+++ lights/clear.py
@@ -0,0 +1,20 @@
+# Simple test for NeoPixels on Raspberry Pi
+import time
+import board
+import neopixel
+
+
+# Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
+# NeoPixels must be connected to D10, D12, D18 or D21 to work.
+pixel_pin = board.D18
+
+# The number of NeoPixels
+num_pixels = 30
+
+ORDER = neopixel.GRB
+
+pixels = neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+)
+
+pixels.fill((0, 000, 0))
