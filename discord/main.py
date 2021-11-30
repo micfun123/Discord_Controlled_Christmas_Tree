76b61e0204370e36d162615bfb5ad7b8094b470d
micfun123 <54761507+micfun123@users.noreply.github.com>
2021-11-30 19:00:54 +0400

    ORDER

--- discord/main.py
+++ discord/main.py
@@ -6,11 +6,17 @@ import discord
 import random
 
 
+# Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
+# NeoPixels must be connected to D10, D12, D18 or D21 to work.
 pixel_pin = board.D18
-
 
 # The number of NeoPixels
 num_pixels = 50
+
+# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+ORDER = neopixel.GRB
+
 
 
 pixels = neopixel.NeoPixel(
