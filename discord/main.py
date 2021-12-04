abef2ab7c87cb1b69837444d4a21daf1189425db
micfun123 <54761507+micfun123@users.noreply.github.com>
2021-12-04 13:16:43 +0400

    Update main.py

--- discord/main.py
+++ discord/main.py
@@ -12,7 +12,7 @@ client = commands.Bot(command_prefix = "%", presences = True, members = True, gu
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game(name = "#help"))
+    await client.change_presence(activity=discord.Game(name = "%help"))
     print("We have power")
     pixels.fill((0, 0, 0))
     pixels.show()
